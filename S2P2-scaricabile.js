fbad89bf80a2903763eee9d9a976cc91969f2386
emanuelebellabarba <161638584+emanuelebellabarba@users.noreply.github.com>
2024-03-24 20:58:37 +0100

    esercizio 7

--- S2P2-scaricabile.js
+++ S2P2-scaricabile.js
@@ -40,20 +40,21 @@ let utenteCheEffettuaLAcquisto = amy //cambia il valore qui per provare se il tu
 
 
 let sommaCarrello = 0
+
 for (let i = 0; i < prices.length; i++) {
-  const prezzo = prices[i]
-  sommaCarrello += prezzo
+  sommaCarrello += prices[i]
 }
 if (utenteCheEffettuaLAcquisto.isAmbassador) {
-  sommaCarrello = (sommaCarrello * 30) / 100
+  sommaCarrello = sommaCarrello - ((sommaCarrello * 30) / 100)
 }
 if (sommaCarrello <= 100) {
   sommaCarrello += shippingCost
 }
+console.log(sommaCarrello)
 
 const tuttiUtenti = []
 tuttiUtenti.push(marco, paul, amy)
-console.log(tuttiUtenti)
+
 for (let i = 0; i < tuttiUtenti.length; i++) {
   const utente = tuttiUtenti[i]
   let stringaBase = `${utente.name} ${utente.lastName} `
